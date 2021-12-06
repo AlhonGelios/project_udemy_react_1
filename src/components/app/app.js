f69d168c393f9b7a521a6f31fdc142d646ae2468
Andrey <geliosal39hon@gmail.com>
2021-12-06 10:08:11 +0300

    lession 90

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,23 @@
-
+import AppInfo from '../app-info/app-info'
+import SearchPanel from '../search-panel/search-panel'
+import AppFilter from '../app-filter/app-filter'
+import EmployessList from '../employees-list/employees-list'
+import EmployeesAddForm from '../employess-add-form/employees-add-form'
 
 import './app.css'
 
 function App() {
     return (
         <div className="app">
+            <AppInfo/>
 
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployessList/>
+            <EmployeesAddForm/>
         </div>
     )
 }
